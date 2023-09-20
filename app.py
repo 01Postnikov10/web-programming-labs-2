9ef778573f0169a13f8476177f1ab81692b10d7a
Кирилл Постников <02k.postnikov@mail.ru>
2023-09-20 13:29:18 +0700

    Меню

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def start():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+        <main>
+            <a href='/lab1'>Первая лабораторная</a>
+        </main>
+        <footer>
+            Постников Кририлл Николаевич, ФБИ-14, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+'''
+
+@app.route('/menu')
+def menu():
+    return 
+
+
+@app.route('/lab1')
+def lab1():     
     return '''
 <!doctype html>
 <html>
@@ -16,9 +45,19 @@ def start():
 
         <h1>web-сервер на flask</h1>
 
+        <div>
+            Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+            называемых микрофреймворков — минималистичных каркасов
+            веб-приложений, сознательно предоставляющих лишь самые ба-
+            зовые возможности.
+        </div>
+
         <footer>
             &copy; Кирилл Постников, ФБИ-14, 3 курс, 2023
         </footer>
     </body>
 </html>
+
 '''
