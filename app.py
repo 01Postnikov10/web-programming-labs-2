16aec615bef495698a321a8adbb523e89a38935c
Кирилл Постников <02k.postnikov@mail.ru>
2023-09-20 14:01:11 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def start():
+    return redirect('/menu', code=302)
+
+@app.route('/menu')
+def menu():
     return '''
 <!doctype html>
 <html>
@@ -24,10 +28,6 @@ def start():
     </body>
 </html>
 '''
-
-@app.route('/menu')
-def menu():
-    return 
 
 
 @app.route('/lab1')
