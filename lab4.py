8f62ac1349d16036447f7977331aaa4df930bffe
Кирилл Постников <02k.postnikov@mail.ru>
2023-11-29 18:25:25 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -5,3 +5,20 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'Kirill' and password == '123':
+        return render_template('sucess1.html', username=username)
+    elif username == '' or password == '':
+        error = 'Заполните оба поля'
+        return render_template('login.html', error=error, username=username, password=password)
+    else:
+        error = 'Неверный логин и/или пароль'
+    return render_template('login.html', error=error, username=username, password=password)
